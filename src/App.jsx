512e18f2b1a336f55b8e35daa851d14967c73430
Alvaro <kanas89@gmail.com>
2021-11-29 11:37:50 +0100

    styled

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Countdown from "./components/Countdown";
+
+function App() {
+  return (
+    <div className="App">
+    <Countdown/>
+    <h2>Wake up, Neo...</h2>
+
+    <a href="https://youtu.be/9ix7TUGVYIo"><img src="https://i.imgur.com/bMDW1Io.png"></img> </a>
+
+    </div>
+  );
+}
+
+export default App;
